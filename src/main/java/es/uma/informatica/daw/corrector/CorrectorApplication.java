ca28557f740de3309962c65511fb39eb51c74bc1
ngoitz <101093006+ngoitz@users.noreply.github.com>
2026-04-08 22:31:49 +0200

    Inicio del proyecto

---
+++ src/main/java/es/uma/informatica/daw/corrector/CorrectorApplication.java
@@ -0,0 +1,13 @@
+package es.uma.informatica.daw.corrector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/uma/informatica/daw/corrector/CorrectorApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uma.informatica.daw.corrector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

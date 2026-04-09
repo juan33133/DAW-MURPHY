0bda8b5ab1595803a3ff69747f1491455e79a01f
juan33133 <juancarballalfdez3@gmail.com>
2026-04-09 16:05:52 +0200

    Metodos setters y getters Corrector y MaterianConvocatoria sustituidos por anotación @Data

--- src/main/java/es/uma/informatica/daw/corrector/models/MateriaEnConvocatoria.java
+++ src/main/java/es/uma/informatica/daw/corrector/models/MateriaEnConvocatoria.java
@@ -1,9 +1,10 @@
 package es.uma.informatica.daw.corrector.models;
 
-import java.util.Objects;
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name = "materias_en_convocatoria")
 public class MateriaEnConvocatoria {
     @Id
@@ -15,37 +16,4 @@ public class MateriaEnConvocatoria {
 
     @Column(name = "id_convocatoria")
     private Long idConvocatoria;
-
-    // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public Long getIdMateria() { return idMateria; }
-    public void setIdMateria(Long idMateria) { this.idMateria = idMateria; }
-    public Long getIdConvocatoria() { return idConvocatoria; }
-    public void setIdConvocatoria(Long idConvocatoria) { this.idConvocatoria = idConvocatoria; }
-
-    // equals, hashCode, toString
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MateriaEnConvocatoria that = (MateriaEnConvocatoria) o;
-        return Objects.equals(id, that.id) &&
-               Objects.equals(idMateria, that.idMateria) &&
-               Objects.equals(idConvocatoria, that.idConvocatoria);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, idMateria, idConvocatoria);
-    }
-
-    @Override
-    public String toString() {
-        return "MateriaEnConvocatoria{" +
-               "id=" + id +
-               ", idMateria=" + idMateria +
-               ", idConvocatoria=" + idConvocatoria +
-               '}';
-    }
 }
